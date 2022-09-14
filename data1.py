3f7c1c93b9ab20c76ab7a3fab00d136e4af48824
zacker232423 <zbray35@gmail.com>
2022-09-14 16:36:12 -0700

    first commit

---
+++ data1.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+nums = [23, 24 ,29 ,14 ,12, 10, 6, 1, 54, 75]
+print(nums [4] )
+
+translations = {'uno':'one', 'treinta y cuatro': 'thirty four'}
+print(translations['uno'])
